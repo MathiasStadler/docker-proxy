a6948f50f0e8fcfdaa22b85a497d44f64d79e125
Mathias Stadler <stadler-mathias@web.de>
2018-02-17 21:17:42 +0100

    save disable-proxy-iptables.sh http part

---
+++ disable-proxy-iptables.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# your proxy IP (docker container)
+SQUIDIP=$(cat .currentContainerIpAddr.txt)
+
+external_interface="eno1"
+
+readonly let HTTP=80
+
+readonly let HTTPS=443
+
+# your proxy listening port
+readonly let SQUIDPORT=3128
+
+# PORT 3120 not working because squid detetct SECURITY ALERT: Host header forgery detected on
+# because squid has no access do nat different namespace
+# NOT WORKING SQUIDPORT_HTTPS=3130
+readonly let SQUIDPORT_HTTPS=3130
+#SQUIDPORT=3129
+# port 80 because the container has a seperate iptable rule to forward the right port
+#SQUIDPORT=80
+
+# TODO set dynamisch interfaces eno1
+#
+# # iptables -t nat -A PREROUTING -i eno1 -s "$SQUIDIP" -p tcp --dport 80 -j ACCEPT
+# # iptables -t nat -A PREROUTING -i eno1 -p tcp --dport 80 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT"
+# # iptables -t nat -A POSTROUTING -j MASQUERADE
+# # iptables -t mangle -A PREROUTING -i eno1 -p tcp --dport "$SQUIDPORT" -j DROP
+
+#https
+# # iptables -t nat -A PREROUTING -i eno1 -s "$SQUIDIP" -p tcp --dport 443 -j ACCEPT
+# # iptables -t nat -A PREROUTING -i eno1 -p tcp --dport 443 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT_HTTPS"
+# # iptables -t nat -A POSTROUTING -j MASQUERADE
+# # iptables -t mangle -A PREROUTING -i eno1 -p tcp --dport "$SQUIDPORT_HTTPS" -j DROP
+
+# TODO remove iptables rule
+
+function error_rule_number(){
+
+
+echo "ERROR :: iptables rule number wring ${1}"
+
+exit 1
+}
+
+
+
+set +x
+# disable
+# iptables -t nat -A PREROUTING -i eno1 -s "$SQUIDIP" -p tcp --dport 80 -j ACCEPT
+while true; do
+    rule_num=$(sudo iptables -t nat -L PREROUTING -n --line-numbers |
+        grep -E "ACCEPT.*tcp.*$SQUIDIP.*tcp.*dpt.*:80.*" |
+        awk '{print $1}' |
+        head -n1)
+
+    [ -z "$rule_num" ] && break
+    echo -en "DELETE RULE => "
+    sudo iptables -t nat -L PREROUTING "${rule_num}"
+    sudo iptables -t nat -D PREROUTING "${rule_num}"
+done
+#
+# disable
+# iptables -t nat -A PREROUTING -i eno1 -p tcp --dport 80 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT"
+while true; do
+    # 'DNAT.*tcp.*eno1.*tcp.*dpt:80.*to.*172.17.0.2:3128'
+    # TODO old echo "DNAT.*tcp.*$external_interface.*tcp.*dpt:$HTTP.*to.*$SQUIDIP:$SQUIDPORT"
+    rule_num=$(sudo iptables -t nat -L PREROUTING -v -n --line-numbers |
+        grep -E "DNAT.*tcp.*$external_interface.*tcp.*dpt:$HTTP.*to.*$SQUIDIP:$SQUIDPORT" |
+        awk '{print $1}' |
+        head -n1)
+
+    [ -z "$rule_num" ] && break
+    echo -en "DELETE RULE => "
+    sudo iptables -t nat -L PREROUTING "${rule_num}"
+    sudo iptables -t nat -D PREROUTING "${rule_num}"
+done
+# disable
+# iptables -t nat -A POSTROUTING -j MASQUERADE
+while true; do
+    # TODO old echo "MASQUERADE.*all.*--.*"
+    rule_num=$(sudo iptables -t nat -L POSTROUTING -v -n --line-numbers |
+        grep -E 'MASQUERADE.*all.*--.*' |
+        awk '{print $1}' |
+        head -n1)
+
+    [ -z "$rule_num" ] && break
+    echo -en "DELETE RULE => "
+    sudo iptables -t nat -L POSTROUTING "${rule_num}"
+    sudo iptables -t nat -D POSTROUTING "${rule_num}"
+done
+# disable
+# iptables -t mangle -A PREROUTING -i eno1 -p tcp --dport "$SQUIDPORT" -j DROP
+while true; do
+# TODO old echo "DROP.*tcp.*$external_interface.*tcp.*dpt:$SQUIDPORT"
+    rule_num=$(sudo iptables -t mangle -L PREROUTING -v -n --line-numbers |
+        grep -E "DROP.*tcp.*$external_interface.*tcp.*dpt:$SQUIDPORT" |
+        awk '{print $1}' |
+        head -n1)
+    [ -z "$rule_num" ] && break
+    echo -en "DELETE RULE => "
+    sudo iptables -t mangle -L PREROUTING "${rule_num}" || error_rule_number ${rule_num}
+    sudo iptables -t mangle -D PREROUTING "${rule_num}" || error_rule_number ${rule_num}
+    # set string rule_num to null, string length null
+    rule_num=""
+    # loop done
+done
+set +x
+
+# TODO old ./show-iptables.sh
--- set-proxy-iptables.sh
+++ set-proxy-iptables.sh
@@ -3,30 +3,34 @@
 # your proxy IP (docker container)
 SQUIDIP=$(cat .currentContainerIpAddr.txt)
 
+# connection to outside the computer's cabinet => Campus Network.
+# TODO find dynamical
+readonly external_interface="eno1"
+
 # your proxy listening port
-SQUIDPORT=3128
+readonly let SQUIDPORT=3128
 
 # PORT 3120 not working because squid detetct SECURITY ALERT: Host header forgery detected on
 # because squid has no access do nat different namespace
 # NOT WORKING SQUIDPORT_HTTPS=3130
-SQUIDPORT_HTTPS=443
+readonly let SQUIDPORT_HTTPS=3130
 #SQUIDPORT=3129
 # port 80 because the container has a seperate iptable rule to forward the right port
 #SQUIDPORT=80
 
 # TODO set dynamisch interfaces eno1
 #
-iptables -t nat -A PREROUTING -i eno1 -s "$SQUIDIP" -p tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -i eno1 -p tcp --dport 80 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT"
-iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t mangle -A PREROUTING -i eno1 -p tcp --dport "$SQUIDPORT" -j DROP
+iptables -t nat -A PREROUTING -i "$external_interface" -s "$SQUIDIP" -p tcp --dport 80 -j ACCEPT
+iptables -t nat -A PREROUTING -i "$external_interface" -p tcp --dport 80 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT"
+iptables -t nat -A POSTROUTING -i "$external_interface" -j MASQUERADE
+iptables -t mangle -A PREROUTING -i "$external_interface" -p tcp --dport "$SQUIDPORT" -j DROP
 
 
 #https
-iptables -t nat -A PREROUTING -i eno1 -s "$SQUIDIP" -p tcp --dport 443 -j ACCEPT
-iptables -t nat -A PREROUTING -i eno1 -p tcp --dport 443 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT_HTTPS"
+iptables -t nat -A PREROUTING -i "$external_interface" -s "$SQUIDIP" -p tcp --dport 443 -j ACCEPT
+iptables -t nat -A PREROUTING -i "$external_interface" -p tcp --dport 443 -j DNAT --to-destination "$SQUIDIP:$SQUIDPORT_HTTPS"
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t mangle -A PREROUTING -i eno1 -p tcp --dport "$SQUIDPORT_HTTPS" -j DROP
+iptables -t mangle -A PREROUTING -i "$external_interface" -p tcp --dport "$SQUIDPORT_HTTPS" -j DROP
 
 #TODO check why https packet dropped
 
